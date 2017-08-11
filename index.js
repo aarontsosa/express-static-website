2982ff50358e6ed197980a654760d5e3d497998a
Aaron Sosa <aarontsosa@gmail.com>
2017-08-11 15:45:27 -0400

    Finishing up for the day

--- index.js
+++ index.js
@@ -1,58 +1,64 @@
 const express = require('express')
 const app = express();
 
-const db = {
-    1001: {
-        author: 'oakly',
-        title: 'my food is not yours',
-        year: 2005
-    },
-    1002: {
-        author: 'chrisaquino',
-        title: 'where is my burbon',
-        year: 2017
-    }
-}
-
-app.get('/books/:bookId', (req, res) => {
-    let bookId = req.params.bookId;
-    let bookInfo = db[bookId];
-    if (!bookInfo) {
-        bookInfo = {
-            "author": "",
-            "year": "",
-            "title": `Book ID ${bookId} not found`
-        }
-    }
-    let respText = `
-    <h1>${bookInfo.title}</h1>
-    <h2>${bookInfo.author}</h2>
-    <h2>${bookInfo.year}</h2>
-    `
-    res.end(respText)
-})
-
-app.get('/books/:bookId/:key', (req, res) => {
-    let bookId = req.params.bookId;
-    let bookInfo = db[bookId];
-    let key = req.params.key
-    if (!bookInfo) {
-        let errorMsg = `Book ID ${bookId} not found`
-        bookInfo = {
-            "author": errorMsg,
-            "year": errorMsg,
-            "title": errorMsg
-        }
-    }
-    let respText = `
-    <h1>${bookInfo.title}</h1>
-    <h2>${bookInfo.author}</h2>
-    <h2>${bookInfo.year}</h2>
-    `
-    res.end(respText)
-})
+app.use(express.static('public/ethereum'))
+app.listen(4900,  () => {
+    console.log('listening on 4900')
+});
 
 
-app.listen(3456, () => {
-    console.log("listening on 3456! AY!")
-})
+// const db = {
+//     1001: {
+//         author: 'oakly',
+//         title: 'my food is not yours',
+//         year: 2005
+//     },
+//     1002: {
+//         author: 'chrisaquino',
+//         title: 'where is my burbon',
+//         year: 2017
+//     }
+// }
+
+// app.get('/books/:bookId', (req, res) => {
+//     let bookId = req.params.bookId;
+//     let bookInfo = db[bookId];
+//     if (!bookInfo) {
+//         bookInfo = {
+//             "author": "",
+//             "year": "",
+//             "title": `Book ID ${bookId} not found`
+//         }
+//     }
+//     let respText = `
+//     <h1>${bookInfo.title}</h1>
+//     <h2>${bookInfo.author}</h2>
+//     <h2>${bookInfo.year}</h2>
+//     `
+//     res.end(respText)
+// })
+
+// app.get('/books/:bookId/:key', (req, res) => {
+//     let bookId = req.params.bookId;
+//     let bookInfo = db[bookId];
+//     let key = req.params.key
+//     if (!bookInfo) {
+//         let errorMsg = `Book ID ${bookId} not found`
+//         bookInfo = {
+//             "author": errorMsg,
+//             "year": errorMsg,
+//             "title": errorMsg
+//         }
+//     }
+//     let respText = `
+//     <h1>${bookInfo.title}</h1>
+//     <h2>${bookInfo.author}</h2>
+//     <h2>${bookInfo.year}</h2>
+//     `
+//     res.end(respText)
+// })
+
+
+// app.listen(3456, () => {
+//     console.log("listening on 3456! AY!")
+// })
